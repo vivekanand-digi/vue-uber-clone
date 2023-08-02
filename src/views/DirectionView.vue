9385523b0a0ca23135553723bfab63c43c7f3e83
vivekanand-digi <117620313+vivekanand-digi@users.noreply.github.com>
2023-08-02 15:38:49 +0530

    added direction-autocomplete-input

---
+++ src/views/DirectionView.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="Directions">
+        <div class="grid grid-cols-12 mt-4 shadow-md">
+            <div class="col-span-2 mx-auto">
+
+                <div>
+                    <ArrowIcon :size="30" />
+                </div>
+
+                <div class="h-16 mt-4 w-full">
+                    <div :class="[isPickupActive ? 'circle-black' : 'circle-gray']"></div>
+                    <div class="line"></div>
+                    <div :class="[!isPickupActive ? 'square-black' : 'square-gray']"></div>
+                </div>
+            </div>
+
+            <div class="col-span-10 pr-10">
+                <div class="w-full h-5"></div>
+                <div class="mb-2 mt-5">
+                    <AutoCompleteInput theId="firstInput" v-model:input="pickup" placeholder="Enter pickup location"
+                        @isActive="isPickupActive = true" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script setup>
+import { ref } from 'vue';
+import AutoCompleteInput from '../components/AutoCompleteInput.vue';
+import ArrowIcon from 'vue-material-design-icons/ArrowLeft.vue';
+
+let isPickupActive = ref(true);
+</script>
+  
+<style lang="scss" scoped>
+#Directions {
+    .bg-custom-color {
+        background-color: rgb(237, 237, 237);
+    }
+
+    .circle-black {
+        margin: 0 auto;
+        width: 9px;
+        height: 9px;
+        background-color: black;
+        border-radius: 100%;
+    }
+
+    .circle-gray {
+        margin: 0 auto;
+        width: 9px;
+        height: 9px;
+        background-color: rgb(191, 191, 191);
+        border-radius: 100%;
+    }
+
+    .square-black {
+        margin: 0 auto;
+        width: 9px;
+        height: 9px;
+        background-color: black;
+
+    }
+
+    .square-gray {
+        margin: 0 auto;
+        width: 9px;
+        height: 9px;
+        background-color: rgb(191, 191, 191);
+    }
+
+    .line {
+        margin: 0 auto;
+        width: 2px;
+        height: 45px;
+        background-color: rgb(191, 191, 191);
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="HOME" class="flex flex-col h-screen">
     <div class="m-3 mb-auto">
+      <div class="my-20">
+        <img width="150" src="img/uber/UberLogo.png" alt="" />
+      </div>
 
       <div class="grid grid-cols-2 gap-3 my-3">
         <ServiceSelectLarge text="Ride" imgageWidth="120" image="ride" />
-        <ServiceSelectLarge text="Food" imgageWidth="120" image="food" promo="true" />
+        <ServiceSelectLarge
+          text="Food"
+          imgageWidth="120"
+          image="food"
+          promo="true"
+        />
       </div>
 
       <div class="grid grid-cols-4 gap-3 my-3">
@@ -14,11 +22,53 @@
         <ServiceSelectSmall text="booked" image="booked" />
       </div>
 
+      <div
+        class="w-full bg-custom-color h-14 rounded-full flex items-center p-3"
+      >
+        <MagnifyIcon :size="40" />
+        <div class="ml-3 text-xs font-semibold text-gray-700">
+          Enter Pick Up Point
+        </div>
+      </div>
     </div>
+
+    <footer
+      class="w-full z-50 flex justify-around p-1 mb-2 border-t-2 border-t-gray-200"
+    >
+      <div class="grid place-items-center">
+        <HomeIcon :size="30" fillColor="#1f1e1e" />
+        <div
+          class="w-full -mt-1 text-xs text-center text-gray-800 font-semibold"
+        >
+          Home
+        </div>
+      </div>
+
+      <div class="grid place-items-center">
+        <MapMarkerIcon :size="30" fillColor="#b3b1b1" />
+        <div
+          class="w-full -mt-1 text-xs text-center text-gray-400 font-semibold"
+        >
+          Destination
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script setup>
-import ServiceSelectSmall from '@/components/ServiceSelectSmall.vue'
-import ServiceSelectLarge from '../components/ServiceSelectLarge.vue'
+import ServiceSelectSmall from "@/components/ServiceSelectSmall.vue";
+import ServiceSelectLarge from "../components/ServiceSelectLarge.vue";
+
+import MagnifyIcon from "vue-material-design-icons/Magnify.vue";
+import HomeIcon from "vue-material-design-icons/Home.vue";
+import MapMarkerIcon from "vue-material-design-icons/MapMarker.vue";
 </script>
+
+<style lang="scss" scoped>
+#Home {
+  .bg-custom-color {
+    background-color: rgb(237, 237, 237);
+  }
+}
+</style>
